36a5aa3b1e8e0451756013baddc0311bca7f94da
Jan Šimonek <jan.simonek@gmail.com>
2022-02-17 18:52:28 +0100

    R - renamed FairArrayTest to IsArrayFairTest

--- src/test/java/IsArrayFairTest.java
+++ src/test/java/IsArrayFairTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class FairArrayTest
+public class IsArrayFairTest
 {
 	private final Solution solution = new Solution();
 

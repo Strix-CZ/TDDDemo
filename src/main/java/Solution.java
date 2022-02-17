94528113303620b2e1e024d54b341327f94af5d3
Jan Šimonek <jan.simonek@gmail.com>
2022-02-17 18:49:29 +0100

    R - Solution generalized all the ifs

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -2,13 +2,12 @@ public class Solution
 {
 	public boolean isFair(int[] values)
 	{
-		if (values.length == 0)
-			return true;
-		else if (values.length == 1)
-			return values[0] == 0;
-		else if (values.length == 2)
-			return values[1] - values[0] == 0;
-		else
-			return values[2] - (values[1] - values[0]) == 0; // I would just do the next step straight away, this is for demonstration purposes :)
+		// Stream.reduce() is not guaranteed to execute sequentially, which we need :(
+
+		int evenMinusOddSum = 0;
+		for (int value : values)
+			evenMinusOddSum = value - evenMinusOddSum;
+
+		return evenMinusOddSum == 0;
 	}
 }
